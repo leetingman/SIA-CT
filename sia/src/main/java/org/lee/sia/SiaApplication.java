943a910831dc08add685641869857ec49dba0a88
leetingman <leetingman@github.com>
2022-11-02 15:17:52 +0900

    first commit&init

---
+++ sia/src/main/java/org/lee/sia/SiaApplication.java
@@ -0,0 +1,13 @@
+package org.lee.sia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiaApplication.class, args);
+	}
+
+}
